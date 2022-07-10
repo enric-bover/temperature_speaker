6a23c1118a6706cc2c42d4eecca43993a4b1607d
enric <ebover.centelles@gmail.com>
2022-07-10 16:58:37 +0200

    El ADC no va be
    Lectures per ADC de diferntes plaques no va bé, deixo el codi d'exemple de lectura a l'espera d'una placa que tingui els ADC bé.

--- temperature_speaker/temperature_speaker.ino
+++ temperature_speaker/temperature_speaker.ino
@@ -1,5 +1,5 @@
 const int sensorPin = A0;
-const float constCleciusPerBit = 165/1024.0;
+const float constCleciusPerBit = 5/1024.0;
 
 
 void setup() {
@@ -9,7 +9,7 @@ void setup() {
 
 void loop() {
   float sensorVal = analogRead(sensorPin);
-  float temperature = sensorVal * constCleciusPerBit - 50;
+  float temperature = (sensorVal * constCleciusPerBit)*100.0 - 50.0;
 
   Serial.println("Sensor Temperature: ");
   Serial.println(temperature);
